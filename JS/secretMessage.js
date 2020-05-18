3fe8bcbd703ef3348e197a31dd0f822277b0da9f
YouMeanThatGuy <paultkeen@gmail.com>
2020-05-18 06:51:58 -0600

    created an array that filtered out unnecessaryWords

--- JS/secretMessage.js
+++ JS/secretMessage.js
@@ -13,3 +13,23 @@ const bigNumbers = [100, 200, 300, 400, 500];
 const smallNumbers = bigNumbers.map(number => {
   return number / 100;
 });
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(number => {
+  return number < 250;
+});
+
+console.log(smallNumbers);
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+})
+
+console.log(longFavoriteWords);
